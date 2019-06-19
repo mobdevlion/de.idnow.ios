3f6a881f44dba55783b3fede289d2bfa49a2e2bf
Alexey Matjuk <bokkusunotegami@gmail.com>
2019-06-19 21:19:05 +0300

    Adds Carthage support

---
+++ IDnowSDKDynamic/IDnowSDKDynamic/IDnowSDKDynamic.h
@@ -0,0 +1,18 @@
+//
+//  IDnowSDK.h
+//  IDnowSDK
+//
+//  Created by Alexey Matjuk on 6/18/19.
+//  Copyright © 2019 IDnow GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+////! Project version number for IDnowSDK.
+FOUNDATION_EXPORT double IDnowSDKVersionNumber;
+//
+////! Project version string for IDnowSDK.
+FOUNDATION_EXPORT const unsigned char IDnowSDKVersionString[];
+
+#import <IDnowSDKDynamic/IDnowSDK.h>
+
